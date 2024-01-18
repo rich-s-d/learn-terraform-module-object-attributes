ac534da84e694803252b1d2740d2e50431833122
Shane Rich <shanerich@Shanes-MacBook-Pro.local>
2024-01-18 11:30:32 +0100

    Introduce object attributes and CORS rules.

--- main.tf
+++ main.tf
@@ -18,6 +18,25 @@ module "website_s3_bucket" {
 
   bucket_prefix = "module-object-attributes-"
 
+  files = {
+    terraform_managed = true
+    www_path          = "${path.root}/www"
+  }
+
+  cors_rules = [
+    {
+      allowed_headers = ["*"],
+      allowed_methods = ["PUT", "POST"],
+      allowed_origins = ["https://test.example.com"],
+      expose_headers  = ["ETag"],
+      max_age_seconds = 3000
+    },
+    {
+      allowed_methods = ["GET"],
+      allowed_origins = ["*"]
+    }
+  ]
+
   tags = {
     terraform     = "true"
     environment   = "dev"
